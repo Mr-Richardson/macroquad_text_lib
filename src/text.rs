1556c6d48e4ce1a4bc613c6642a3e3c32957e052
Mr_Richardson <mr.richardson@outlook.de>
2026-04-19 19:32:09 +0200

    initial commit

---
+++ src/text.rs
@@ -0,0 +1,50 @@
+use macroquad::color::Color;
+use macroquad::prelude::measure_text;
+use macroquad::text::{draw_text, Font};
+
+enum AlignX {
+Left,
+Center,
+Right,
+}
+
+enum AlignY {
+Top,
+Center,
+Bottom,
+}
+
+struct Text {
+    font: Font,
+    align_x: AlignX,
+    align_y: AlignY,
+    size: u16,
+    color: Color,
+}
+
+impl Text {
+    fn new(font: Font, align_x: AlignX, align_y: AlignY, size: u16, color: Color) -> Text {
+        Text {
+            font,
+            align_x,
+            align_y,
+            size,
+            color,
+        }
+    }
+
+    fn draw(&self, text: &str, mut x: f32, mut y: f32, max_w: f32) {
+        let dimensions = measure_text(text, Some(&self.font), self.size, 1.0);
+        match self.align_x {
+            AlignX::Center => x -= dimensions.width / 2.0,
+            AlignX::Right => x -= dimensions.width,
+            _ => (),
+        }
+        match self.align_y {
+            AlignY::Center => y -= dimensions.offset_y / 2.0,
+            AlignY::Top => y -= dimensions.offset_y,
+            _ => (),
+        }
+        draw_text(text, x, y, self.size as f32, self.color);
+    }
+}
